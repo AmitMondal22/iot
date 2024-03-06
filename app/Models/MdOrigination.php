e231193e641adb932105152dfed96a5b28775ff9
meettan <meettan@synergicsoftek.com>
2024-03-06 08:28:38 +0530

    no change

--- app/Models/MdOrigination.php
+++ app/Models/MdOrigination.php
@@ -10,14 +10,11 @@ class MdOrigination extends Model
     use HasFactory;
     protected $table = 'md_origination';
     protected $primaryKey = 'origination_id';
-    protected $fillable = ["origination_name", "active_status", "create_by"];
+    protected $fillable = [ "origination_name","active_status","create_by"];
 
 
     public function assign_devices()
     {
-        return $this->hasMany(TdAssignDevice::class, 'origination_id', 'origination_id')
-            ->join("users as a","a.id","=","td_assign_device.assign_user_id")
-            ->select('td_assign_device.*', 'users.*')
-            ->get();
+        return $this->hasMany(TdAssignDevice::class, 'origination_id', 'origination_id');
     }
 }
